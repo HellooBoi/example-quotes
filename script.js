7a4dd6fcf30fe0d662fe7a8c269a76e45f616a54
HellooBoi <105688047+HellooBoi@users.noreply.github.com>
2023-07-16 06:12:23 +0700

    13 tweet and newQuote

--- script.js
+++ script.js
@@ -1,22 +1,66 @@
-// let apiQuotes = [];
+const quoteContainer = document.getElementById('quote-generator');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
 
-// Show New Quote
-function newQuote() {
-    // Pick a random quote from apiQuote array
-    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-    console.log(quote);
-}
-// Get Quotes From API
-// async function getQuotes() {
-//     const apiUrl = 'https://type.fit/api/quotes';
-//     try {
-//         const response = await fetch(apiUrl);
-//         apiQuotes = await response.json();
-//         newQuote();
-//     } catch (error) {
-//         // Catch Error Here
-//     }
+let apiQuotes = [];
+
+// Show New Quote----
+
+// LocalQuotes----
+// function newQuote() {
+//     // Pick a random quote from apiQuote array
+//     const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+//     console.log(quote);
 // }
-// // On Load
-// getQuotes();
-newQuote();
+
+function newQuote() {
+    // Pick a random quote from apiQuote array----
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    // Check if Author field is blank and replace it with 'Unknown'
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+
+    // Check Quote length to determine styling---- ปรับ font size auto มีตัวหนังสือน้อยจะตัวใหญ่ขึ้น
+    if (quote.text.length > 50) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    
+    quoteText.textContent = quote.text;
+}
+
+// Get Quotes From API----
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        // Catch Error Here
+    }
+}
+
+
+// LocalQuote----
+// newQuote();
+
+// Tweet Quote----
+function tweetQuote(){
+    const twitterUtl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUtl, '_blank');
+    // ใน VDO เป็น twitterUrl น่าจะเปลี่ยนใหม่
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// On Load----
+getQuotes();
